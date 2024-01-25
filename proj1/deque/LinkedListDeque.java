26750acd741d84ea224aa2aef2d90030b14bd826
cnkiro <365628724@qq.com>
2024-01-25 16:25:41 +0800

    find some bugs

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -155,29 +155,4 @@ public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
         }
         return true;
     }
-
-    public static void main(String[] args) {
-        int n = 99;
-
-        LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
-        for (int i = 0; i <= n; i++) {
-            lld1.addLast(i);
-        }
-
-        LinkedListDeque<Integer> lld2 = new LinkedListDeque<>();
-        for (int i = n; i >= 0; i--) {
-            lld2.addFirst(i);
-        }
-
-        lld1.printDeque();
-
-        System.out.println(lld1.equals(lld2));
-
-        ArrayDeque<Integer> ad1 = new ArrayDeque<>();
-        for (int i = 0; i <= n; i++) {
-            ad1.addLast(i);
-        }
-
-        System.out.println(lld1.equals(ad1));
-    }
 }
--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,27 +3,34 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    private Comparator<T> cp;
 
-    private T maxItem;
     public MaxArrayDeque(Comparator<T> c) {
         super();
-        maxItem = max(c);
+        cp = c;
     }
 
     public T max() {
-        if (size() == 0) {
+        if (isEmpty()) {
             return null;
         }
-        return maxItem;
+        int maxIndex = 0;
+        for (int i = 0; i < size(); i++)
+            if (cp.compare(this.get(i), this.get(maxIndex)) > 0) {
+                maxIndex = i;
+        }
+        return this.get(maxIndex);
     }
 
     public T max(Comparator<T> c) {
+        if (isEmpty()) {
+            return null;
+        }
         int maxIndex = 0;
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size(); i++)
             if (c.compare(this.get(i), this.get(maxIndex)) > 0) {
                 maxIndex = i;
             }
-        }
         return this.get(maxIndex);
     }
 }
